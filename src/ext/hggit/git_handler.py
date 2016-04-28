a7a2a489fd082c14ea89da956b0e475319b373da
see <none@none>
2016-04-28 08:03:11 +0100

    fix

--- src/ext/hggit/git_handler.py
+++ src/ext/hggit/git_handler.py
@@ -1148,7 +1148,7 @@ class GitHandler(object):
                 f = cStringIO.StringIO()
                 ret = localclient.fetch_pack(path, determine_wants, graphwalker,
                                              f.write, progress.progress)
-                if(f.pos != 0):
+                if(f.tell() != 0):  #cwp
                     f.seek(0)
                     self.git.object_store.add_thin_pack(f.read, None)
                 progress.flush()
